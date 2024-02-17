945dc76e0ddbf364f2db32f9bb0d61743d3ee49e
Your Name <you@example.com>
2024-02-17 23:51:22 +0000

    Fix docker script

--- poky/docker.sh
+++ poky/docker.sh
@@ -1,1 +1,1 @@
-sudo docker run -v "$(pwd)/../../new-opensource":/var/build/opensource -it vaddio/yocto-16.04:16.04-latest /bin/bash
+sudo docker run -v "$(pwd)/../":/var/build/opensource -it vaddio/yocto-16.04:16.04-latest /bin/bash
